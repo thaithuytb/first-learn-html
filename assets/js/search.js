87ce12c827407d8625d26c26ee5be0cdd682ae79
thaithuytb <ngoquangthai29112001@gmail.com>
2020-10-03 10:35:50 +0700

    test

---
+++ assets/js/search.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){
+    var x1 = document.getElementsByClassName('login');
+    x1[0].onclick=function(){
+        x1[0].classList.add('left');
+        console[0].log(x1);
+    }
+}, false);
