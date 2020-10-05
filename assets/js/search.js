452cc3139160c0d911080614c0ffec2ced189170
nguyenthingan20191985 <nguyenthingan0803@gmail.com>
2020-10-05 09:12:26 +0700

    cj ngan gui em thai

--- assets/js/search.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){
-    var x1 = document.getElementsByClassName('login');
-    x1[0].onclick=function(){
-        x1[0].classList.add('left');
-        console[0].log(x1);
-    }
-}, false);
